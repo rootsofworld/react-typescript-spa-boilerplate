f09fc46e5a5f46bf5ad281d6fd56ad3c9f55cc45
rootsofworld <hsfo3o@gmail.com>
2022-06-15 00:52:24 +0800

    Change project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import LoginForm from "@/components/loginform";
 
 function App() {
     return (
         <div className='App'>
             <p className='text-slate-400'>Hello Vite + React!</p>
-            <LoginForm method='get'></LoginForm>
         </div>
     );
 }
